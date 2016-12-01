af9df1ec6973194d3aa89e497dbd82417991dea1
tarilabs <matteo.mortari@gmail.com>
2016-12-01 16:30:37 +0100

    Fixed by just changing the JUnit's @Parameters name string avoid {1}.

--- kie-dmn-feel/src/test/java/org/kie/dmn/feel/runtime/FEELStringOperationsTest.java
+++ kie-dmn-feel/src/test/java/org/kie/dmn/feel/runtime/FEELStringOperationsTest.java
@@ -16,44 +16,26 @@
 
 package org.kie.dmn.feel.runtime;
 
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertThat;
-
 import java.util.Arrays;
 import java.util.Collection;
-
-import org.junit.Test;
 import org.junit.runners.Parameterized;
-import org.kie.dmn.feel.FEEL;
 
-public class FEELStringOperationsTest {
-    
-    private final FEEL feel = FEEL.newInstance();
+public class FEELStringOperationsTest extends BaseFEELTest {
     
     /**
-     * Avoid using JUnit's @Parameterized runner as String escape would trigger problem for rendering the actual test method
-     * for instance in JUnit runner IDE GUIs, and in surefire-report which would cause further problem in Jenkins.
+     * WARNING: do not use as JUNit's @Parameters name the index {1} within this test class, as this would result in invalid character in the XML surefire-report
+     * Original error was: An invalid XML character (Unicode: 0x8) was found in the value of attribute "name" and element is "testcase".
      */
-    @Test
-    public void testStringOperations() {
-        // string concatenation
-        assertResult("\"foo\"+\"bar\"", "foobar");
-        
-        // string escapes
-        assertResult( "\"string with \\\"quotes\\\"\"",          "string with \"quotes\""   );
-        assertResult( "\"a\\b\\t\\n\\f\\r\\\"\\'\\\\\\u2202b\"", "a\b\t\n\f\r\"\'\\\u2202b" );
-    }
-    
-    private void assertResult( String expression, Object result ) {
-        if( result == null ) {
-            assertThat( "Evaluating: '" + expression + "'", feel.evaluate( expression ), is( nullValue() ) );
-        } else if( result instanceof Class<?> ) {
-            assertThat( "Evaluating: '" + expression + "'", feel.evaluate( expression ), is( instanceOf( (Class<?>) result ) ) );
-        } else {
-            assertThat( "Evaluating: '"+expression+"'", feel.evaluate( expression ), is( result ) );
-        }
+    @Parameterized.Parameters(name = "{index}: {0} ")
+    public static Collection<Object[]> data() {
+        final Object[][] cases = new Object[][] {
+                // string concatenation
+                { "\"foo\"+\"bar\"", "foobar" },
+                // string escapes
+                { "\"string with \\\"quotes\\\"\"", "string with \"quotes\""},
+                { "\"a\\b\\t\\n\\f\\r\\\"\\'\\\\\\u2202b\"", "a\b\t\n\f\r\"\'\\\u2202b"}
+        };
+        return Arrays.asList( cases );
     }
 }
 
